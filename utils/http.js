b24e86c29958247b061f03cf27438bad00430922
Michael Burdi <maburdi@gmail.com>
2020-04-28 17:09:24 -0700

    Update HTTP utils - Remove static asset functions - Add token payload function

--- utils/http.js
+++ utils/http.js
@@ -1,44 +1,22 @@
 
-let fs = require('fs');
 let qs = require('querystring');
-let path = require('path');
-
 let jwt = require('jsonwebtoken');
 
 let {SECRET} = require('../config');
 
-let staticRoot = __dirname;
-
-module.exports.getAsyncFileStream = function getAsyncFileStream(filePath) {
-    return new Promise((resolve, reject) => {
-        let fileStream = fs.createReadStream(path.join(staticRoot, filePath));
-        fileStream.once('error', reject);
-        fileStream.once('readable', () => {
-            console.debug(path.join(staticRoot, filePath));
-            resolve(fileStream)
-        });
-    });
-};
-
-
-
-module.exports.serveStaticAssets = function serveStaticAssets(filePath) {
-    staticRoot = filePath;
-};
-
-
-
-module.exports.getPostData = function(request, response) {
+module.exports.getPostData = function(request, response, asObject = true) {
     return new Promise((resolve, reject) => {
         let body = [];
         request.on('data', (chunk) => body.push(chunk));
         request.on('error', (err) => reject(err));
         request.on('end', () => {
             body = Buffer.concat(body).toString();
-            try {
-                body = JSON.parse(body); // JSON string
-            } catch (e) {
-                body = qs.parse(body); // URL Encoded
+            if (asObject) {
+                try {
+                    body = JSON.parse(body); // JSON string
+                } catch (e) {
+                    body = qs.parse(body); // URL Encoded
+                }
             }
             resolve(body);
         });
@@ -49,7 +27,7 @@ module.exports.getPostData = function(request, response) {
 module.exports.getTokenPayload = function(request, response) {
     let auth = request.headers.authorization || String();
     let match = /^Bearer ([-.\w]+)/.exec(auth);
-    return jwt.decode(match);
+    return jwt.decode(match[1]);
 };
 
 
@@ -57,7 +35,6 @@ module.exports.auth = function(request, response) {
 
     try {
         let auth = request.headers.authorization || String();
-
         let match = /^Bearer ([-.\w]+)/.exec(auth);
 
         if (match) {
